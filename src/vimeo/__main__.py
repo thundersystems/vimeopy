f959ee31d960eaff1661cb8998184ff0bf079ae1
Benedetto <none@none>
2016-07-05 11:04:39 +0200

    First dummy test

--- src/vimeo/__main__.py
+++ src/vimeo/__main__.py
@@ -1,16 +1,82 @@
-"""
-Entrypoint module, in case you use `python -mvimeo`.
+# coding: utf-8
 
-
-Why does this file exist, and why __main__? For more info, read:
-
-- https://www.python.org/dev/peps/pep-0338/
-- https://docs.python.org/2/using/cmdline.html#cmdoption-m
-- https://docs.python.org/3/using/cmdline.html#cmdoption-m
-"""
+import getopt
+import logging
 import sys
+import unittest
 
-from vimeo.cli import main
+from vimeo.logger import LoggerSingleton
+LOGGER_SINGLETON = LoggerSingleton()
+LOG_LEVEL_DICT = {
+    'CRITICAL': 50,
+    'ERROR': 40,
+    'WARNING': 30,
+    'INFO': 20,
+    'DEBUG': 10,
+    'NOTSET': 0,
+}
 
-if __name__ == "__main__":
-    sys.exit(main())
+# STATIC VAR
+LOG_LEVEL = None
+LOGGER_ENABLED = False
+TOKEN = None
+
+
+class TestVimeopyBase(unittest.TestCase):
+    def __init__(self, methodName='runTest'):
+
+        # CLIENT
+        from vimeo.clients import VimeoClientSingleton
+        self.client = VimeoClientSingleton(
+            token=TOKEN,
+            logger_enabled=LOGGER_ENABLED,
+            logger_level=LOG_LEVEL,
+        )
+
+        # SETTING LOG
+        LOGGER_SINGLETON.setLevel(LOG_LEVEL)
+        if LOGGER_ENABLED:
+            LOGGER_SINGLETON.disabled = False
+        else:
+            LOGGER_SINGLETON.disabled = True
+        super(TestVimeopyBase, self).__init__(methodName)
+
+
+class TestReadUser(TestVimeopyBase):
+    def test_dummy(self):
+        try:
+            self.client.read_user()
+        except Exception as e:
+            LOGGER_SINGLETON.error(e.error_text)
+        return True
+
+
+if __name__ == '__main__':
+
+    opts, args = getopt.getopt(sys.argv[1:], '', [
+        'token=',
+        'log-level=',
+        'logger-enabled',
+    ])
+
+    for option, value in opts:
+        if option == '--token':
+            TOKEN = value
+        if option == '--logger-enabled':
+            LOGGER_ENABLED = True
+        if option == '--log-level':
+            LOGGER_ENABLED = True
+            if value in LOG_LEVEL_DICT.keys():
+                LOG_LEVEL = LOG_LEVEL_DICT[value]
+            if value in LOG_LEVEL_DICT.values():
+                LOG_LEVEL = value
+
+    if not LOG_LEVEL:
+        LOG_LEVEL = LOG_LEVEL_DICT['INFO']
+
+    if not TOKEN:
+        LOGGER_SINGLETON.error('Token is required')
+    else:
+        del sys.argv[1:]
+        unittest.main()
+
